1cecfe0d290d42ddbc5d3cc8392401f0fd320846
Niraj Pandkar <niraj.pandkar@gmail.com>
2020-04-20 10:00:48 +0530

    (functionality) Changed the input and output to stdin and stdout instead of files

--- heirarchy.py
+++ heirarchy.py
@@ -1,13 +1,10 @@
 import re
 import argparse
 import os
+import sys
 
-def modify(filename):
-    os.system("cp " + filename + " " + filename + ".bak")    
+def modify(text):  
     flag = False
-
-    with open(filename, "r") as infile:
-        text = infile.readlines()
 
     loc = dict()
     inv_text = text[::-1]
@@ -24,24 +21,15 @@ def modify(filename):
 
 
     finaltext = ""
-    with open(filename, "r") as infile:
-        text = infile.readlines()
-        # os.system("rm " + filename)
-        for i in range(len(text)-1, -1, -1):
-            modline = ""
-            if loc[i]:
-                modline = "[] " + text[::-1][i]
-            else:
-                modline = text[::-1][i]
-            finaltext += modline
-    with open(filename, "w") as outfile:
-        outfile.write(finaltext)
+    for i in range(len(text)-1, -1, -1):
+        modline = ""
+        if loc[i]:
+            modline = "[] " + text[::-1][i]
+        else:
+            modline = text[::-1][i]
+        finaltext += modline + "\n"
+    sys.stdout.write(finaltext)
 
 if __name__ == "__main__":
-    # construct the argument parse and parse the arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-f", "--file", required=True,
-        help="path/name of the file")
-    args = vars(ap.parse_args())
-    
-    modify(args["file"])
+    text = sys.stdin.read()
+    modify(text.split("\n"))
